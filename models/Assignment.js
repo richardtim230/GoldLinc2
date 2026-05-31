5b79551bf503d2809e5d5e22a450f3ea78f6de4f
richardtim235 <ochukorichard@student.oauife.edu.ng>
2026-05-31 11:24:43 +0100

    Update Assignment.js

--- models/Assignment.js
+++ models/Assignment.js
@@ -2,19 +2,66 @@ const mongoose = require('mongoose');
 
 const AssignmentSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  description: { type: String, default: "" },
-  cbt: { type: mongoose.Schema.Types.ObjectId, ref: 'Exam', required: false },
-  subject: { type: mongoose.Schema.Types.ObjectId, ref: 'Subject', required: true },
-  assignedTo: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Student' }],
-  class: { type: mongoose.Schema.Types.ObjectId, ref: 'Class', required: true },
-  teacher: { type: mongoose.Schema.Types.ObjectId, ref: 'Staff', required: true }, // <--- ADD THIS LINE
-  files: [{ url: String, name: String }],
-  dueDate: { type: Date, required: true },
 
-cbt: { type: mongoose.Schema.Types.ObjectId, ref: 'Exam', required: false },
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'Staff' },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now }
+  description: {
+    type: String,
+    default: ""
+  },
+
+  type: {
+    type: String,
+    enum: ['STANDARD', 'QUESTION_BANK'],
+    default: 'STANDARD'
+  },
+
+  questionsAllocated: [{
+    type: String
+  }],
+
+  cbt: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Exam',
+    required: false
+  },
+
+  subject: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Subject',
+    required: true
+  },
+
+  assignedTo: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Student'
+  }],
+
+  class: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Class',
+    required: true
+  },
+
+  teacher: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Staff',
+    required: true
+  },
+
+  files: [{
+    url: String,
+    name: String
+  }],
+
+  dueDate: {
+    type: Date,
+    required: true
+  },
+
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Staff'
+  }
+
 }, { timestamps: true });
 
 module.exports = mongoose.model('Assignment', AssignmentSchema);
